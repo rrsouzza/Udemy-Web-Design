edff1b8e4ae19052d21bfe631f796fb10ff91ddc
Ricardo Souza <rr_souza@outlook.com>
2022-02-02 18:43:57 -0300

    Item 326-353 - Projeto Final finalizado

--- Item 326-353 - Projeto Final/js/main.js
+++ Item 326-353 - Projeto Final/js/main.js
@@ -8,4 +8,18 @@
         toggleBtn: '.header__btnMenu',
         widthEnabled: 1024
     })
+
+    var carouselImgs = new Carousel({
+        container: '.laptop-slider .slideshow',
+        itens: 'figure',
+        btnPrev: '.prev',
+        btnNext: '.next'
+    })
+
+    var carouselQuotes = new Carousel({
+        container: '.quote-slideshow',
+        itens: 'figure',
+        btnPrev: '.prev',
+        btnNext: '.next'
+    })
 })()
